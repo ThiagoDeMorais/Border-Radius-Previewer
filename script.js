a671fc6195ac69c69fbf518942e1afe33cc8789a
Thigo De Morais <demoraisthiago63@gmail.com>
2021-02-08 18:27:24 -0300

    Mudanca do nome de algumas funcoes

--- script.js
+++ script.js
@@ -1,36 +1,18 @@
 /* var x = document.getElementById("box");*/
+function changeTopLeft(){
+    let topLeftController = document.getElementById("topLeftSlider");
+    let topLeftRadius = document.getElementById("box");
 
-function changeRight(){
-    let rightController = document.getElementById("rightslider");
-    let rightRadius = document.getElementById("box");
-    console.log("radius: " + rightRadius.style.borderTopRightRadius);
-    console.log("Control value: " + rightController.value);
-
-    rightRadius.style.borderTopRightRadius = rightController.value + "%";
-    console.log("radius: " +rightRadius.style.borderTopRightRadius);
-    /*
-    x.style.borderTopRightRadius = document.getElementById("rightslider").value + "%"; 
-    console.log(x.style.borderTopRightRadius);
-    */
+    topLeftRadius.style.borderTopLeftRadius = topLeftController.value + "%";
 }
 
 
-function changeLeft(){
-    let leftController = document.getElementById("leftslider");
-    let leftRadius = document.getElementById("box");
-    console.log("radius: " + leftRadius.style.borderTopRightRadius);
-    console.log("Control value: " + leftController.value);
+function changeTopRight(){
+    let topRightController = document.getElementById("topRightslider");
+    let topRightRadius = document.getElementById("box");
 
-    leftRadius.style.borderTopLeftRadius = leftController.value + "%";
-    console.log("radius: " +leftRadius.style.borderTopLeftRadius);
+    topRightRadius.style.borderTopRightRadius = topRightController.value + "%";
+
 }
 
-function changeTop(){
-    let topController = document.getElementById("topslider");
-    let toptRadius = document.getElementById("box");
-    console.log("radius: " + toptRadius.style.borderTopRightRadius);
-    console.log("Control value: " + leftController.value);
 
-    leftRadius.style.borderTopLeftRadius = leftController.value + "%";
-    console.log("radius: " +leftRadius.style.borderTopLeftRadius);
-}
